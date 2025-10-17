f2baed0a8e6f4e88113dbce4e0634e654049ec1a
hrz <herz23@mails.tsinghua.edu.cn>
2025-10-17 16:30:48 +0000

    1

--- models/agent_swin_diff.py
+++ models/agent_swin_diff.py
@@ -113,31 +113,6 @@ def window_reverse(windows, window_size, H, W):
     return x
 
 
-class PosCNN(nn.Module):
-    def __init__(self, in_chans, embed_dim=768, s=1):
-        super(PosCNN, self).__init__()
-        self.proj = nn.Sequential(
-            nn.Conv2d(in_chans, embed_dim, 3, s, 1, bias=True, groups=embed_dim),
-        )
-        self.s = s
-
-    def forward(self, x):
-        B, N, C = x.shape
-        H = int(N**0.5)
-        W = int(N**0.5)
-        feat_token = x
-        cnn_feat = feat_token.transpose(1, 2).view(B, C, H, W)
-        if self.s == 1:
-            x = self.proj(cnn_feat) + cnn_feat
-        else:
-            x = self.proj(cnn_feat)
-        x = x.flatten(2).transpose(1, 2)
-        return x
-
-    def no_weight_decay(self):
-        return ["proj.%d.weight" % i for i in range(4)]
-
-
 class WindowAttention(nn.Module):
     r"""Window based multi-head self attention (W-MSA) module with relative position bias.
     It supports both of shifted and non-shifted window.
@@ -301,12 +276,10 @@ class AgentAttention(nn.Module):
         self.proj_drop = nn.Dropout(proj_drop)
         self.softmax = nn.Softmax(dim=-1)
         self.shift_size = shift_size
-
         self.agent_num = agent_num
         self.dwc = nn.Conv2d(
             in_channels=dim, out_channels=dim, kernel_size=(3, 3), padding=1, groups=dim
         )
-
         pool_size = int(agent_num**0.5)
         self.pool = nn.AdaptiveAvgPool2d(output_size=(pool_size, pool_size))
 
